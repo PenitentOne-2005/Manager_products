fcc18185c95bef77185a497c2233e3be158e4775
PenitentOne-2005 <pozdrakovzena@gmail.com>
2025-10-28 23:52:29 +0300

    .

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,7 +1,32 @@
-import "./App.css";
+import type { FC } from "react";
+import { useProducts } from "./hooks";
+import { ListItems, CustomForm } from "./components";
+import classes from "./styles/main.module.css";
 
-function App() {
-  return <></>;
-}
+const App: FC = () => {
+  const { data, loading, error, success, createProduct, setSuccess, setError } =
+    useProducts();
+
+  return (
+    <div>
+      <h2 className={classes.title}>Додати продукт</h2>
+
+      <CustomForm
+        createProduct={createProduct}
+        setSuccess={setSuccess}
+        setError={setError}
+        loading={loading}
+      />
+
+      {success && <p className={classes.successMessage}>{success}</p>}
+      {error && <p className={classes.errorMessage}>{error}</p>}
+
+      <h3 className={classes.title}>Список продуктів</h3>
+
+      {loading && <p className={classes.loading}>Завантаження...</p>}
+      {!loading && !error && <ListItems data={data} />}
+    </div>
+  );
+};
 
 export default App;
